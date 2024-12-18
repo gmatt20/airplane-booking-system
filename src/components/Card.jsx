afdfe4f0ab3150b247f20b8615e03d6fa509ebe7
Matthew Garcia <gmatthew2765@gmail.com>
2024-12-17 22:24:10 -0800

    Added images for Card componenets. Finished basic card componenets.

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+function Card( {destinations} ){
+  return(
+    <>
+    <article>
+        <div className="flex justify-center gap-5 flex-column">
+            {destinations.map((element, index) => (
+              <div key={index} className="mb-2 h-56 w-96 min-w-60 max-h-40 p-2 rounded-lg text-white shadow-[inset_0_80px_13px_-15px_rgba(17,15,79,0.5)]"
+              style={{
+                backgroundImage: `url(${element.img})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              >
+                <h1 className="font-extrabold">{element.city}</h1>
+                <p className="text-sm">Average price: ${element.price}</p>
+              </div>
+            ))}
+        </div>
+      </article>
+    </>
+  );
+}
+
+export default Card;
--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -1,8 +1,41 @@
+import Card from "./Card.jsx";
+
 function MainArticle(){
+  const trendingDestinations = [
+    {
+      city: "Orlando",
+      price: 174,
+      img: "/public/orlando.jpg",
+    },
+    {
+      city: "Las Vegas",
+      price: 218,
+      img: "/public/lasvegas.jpg",
+    },
+    {
+      city: "New York",
+      price: 491,
+      img: "/public/newyork.jpg",
+    },
+    {
+      city: "Atlanta",
+      price: 153,
+      img: "/public/atlanta.jpg",
+    },
+    {
+      city: "Tokyo",
+      price: 223,
+      img: "/public/tokyo.jpg",
+    },
+  ];
+
   return(
     <>
-      <h1>Popular flights near you</h1>
-      <p>Domestic and international flights</p>
+      <main className="m-5">
+          <h1>Popular flights near you</h1>
+          <p>Domestic and international flights</p>
+          <Card destinations = {trendingDestinations}/>
+      </main>
     </>
   );
 }
