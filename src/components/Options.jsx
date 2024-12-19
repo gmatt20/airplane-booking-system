aa88b025727dc4f8c98aed34bd0a0c117f07c785
Matthew Garcia <gmatthew2765@gmail.com>
2024-12-18 16:39:08 -0800

    Made simple AllSeats page UI.

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,8 +2,6 @@ import { Link, useLocation } from "react-router-dom";
 
 function Options( {buttonLabels} ){
   const location = useLocation();
-
-  console.log(location);
   
   return(
     <>
--- src/pages/AllSeats.jsx
+++ src/pages/AllSeats.jsx
@@ -1,10 +1,31 @@
 import Header from "../components/Header";
+import Icon from '@mdi/react';
+import { mdiSeat } from '@mdi/js';
 
 function AllSeats(){
+  let seats = [];
+
+  for(let i = 1; i <= 5; i++){
+    for(let j = 65; j <= 70; j++){
+      let seat = { id: `${i}${String.fromCharCode(j)}`, status: true,};
+      seats.push(seat);
+    }
+  }
+
   return(
     <>
       <Header></Header>
-      <h1>View All Seats</h1>
+      <div className="p-5">
+        <h1>View All Seats</h1>
+        <ul className="grid grid-cols-6 place-items-center gap-x-3 px-44">
+          {seats.map((seat) => (
+            <li className="flex w-fit" key={seat.id}><Icon path={mdiSeat} 
+            size={1}
+            color={seat.status ? "green" : "red"}
+            />{seat.id}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
